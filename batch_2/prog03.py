ed26c5b04f4f3b80dfe8d7f337d3fa432a89db7e
izartorius <atreuseinzbelt@gmail.com>
2025-03-13 18:22:57 +0800

    added if statements that checks which of the inputs are bigger and uses the corresponding variable for calculation then directly prints the print statement

--- batch_2/prog03.py
+++ batch_2/prog03.py
@@ -2,3 +2,8 @@ num1 = float(input("Enter a number: "))
 num2 = float(input("Enter a number: "))
 diff = num1 - num2
 diff2 = num2 - num1
+
+if num1 < num2:
+    print(f"The difference of both numbers is: {diff2}")
+else:
+    print(f"The difference of both numbers is: {diff}")
