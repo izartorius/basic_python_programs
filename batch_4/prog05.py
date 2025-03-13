b98f8fbfa5cb93ad49ac10f06ef6d084652c4435
izartorius <atreuseinzbelt@gmail.com>
2025-03-13 23:30:43 +0800

    added variables to calculate for the list's average

--- batch_4/prog05.py
+++ batch_4/prog05.py
@@ -4,3 +4,7 @@ while True:
     try:
         num = float(input("Enter a number: "))
         inputs.append(num)
+
+        total = sum(inputs)
+        count = len(inputs)
+        average = total / count
