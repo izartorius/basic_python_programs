6387d23456370752e1b080d5726ef68b41912d4e
izartorius <atreuseinzbelt@gmail.com>
2025-03-13 20:27:11 +0800

    added if statements that checks the bigger number among both inputs and finds the numbers between those 2 inputs using for loop then directly adds them to the num list which are then displayed in the print statement using f string

--- batch_2/prog10.py
+++ batch_2/prog10.py
@@ -2,3 +2,11 @@ num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
 num = []
 
+if num1 > num2:
+    for i in range(num2 + 1, num1):
+        num.append(i)
+    print(f"The numbers between {num1} and {num2} is {num}")
+elif num1 < num2:
+    for i in range(num1 +1, num2):
+        num.append(i)
+    print(f"The numbers between {num1} and {num2} is {num}")
