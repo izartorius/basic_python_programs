ca98d85ae4832486f92589bf8103a13214d62ffa
izartorius <atreuseinzbelt@gmail.com>
2025-03-13 16:55:37 +0800

    added a for loop which asks for the remaining inputs and assigned an addition operator to num to prevent incomplete addition

--- batch_1/prog07.py
+++ batch_1/prog07.py
@@ -3,3 +3,8 @@ num2 = float(input("Enter the 2nd number: "))
 num3 = float(input("Enter the 3rd number: "))
 total = num1 + num2 + num3
 
+for i in range(4, 11):
+    num = float(input(f"Enter the {i}th number: "))
+    total += num
+
+print(f"The sum of all numbers is {total}")
