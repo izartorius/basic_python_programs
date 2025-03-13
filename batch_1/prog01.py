6e422e3446659b2f8597583df1e177aab9334ea5
izartorius <atreuseinzbelt@gmail.com>
2025-03-13 15:58:00 +0800

    prints the bigger number out of 2 inputs

---
+++ batch_1/prog01.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+if num1 > num2:
+    print(num1)
+elif num1 < num2:
+    print(num2)
+else:
+    print("Both numbers are equal")
