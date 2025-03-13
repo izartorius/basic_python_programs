5b1c73cb940c8086264085b44f9a6fe946194753
izartorius <atreuseinzbelt@gmail.com>
2025-03-13 21:59:26 +0800

    added while loop that constantly asks for a user input and if statements which checks if the input is already on the list

--- batch_3/prog03.py
+++ batch_3/prog03.py
@@ -1,2 +1,11 @@
 inputs = []
 
+while True:
+    try:
+        num = float(input("Enter a number: "))
+
+        if num in inputs:
+            print("Duplicate")
+        else:
+            print("Unique")
+            inputs.append(num)
