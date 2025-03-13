13b2851e38c324b22589bb66bece1520727013cc
izartorius <atreuseinzbelt@gmail.com>
2025-03-13 23:16:14 +0800

    added a print statement that displays the highest input in the list when the except statement run. then breaks the loop

--- batch_4/prog03.py
+++ batch_4/prog03.py
@@ -4,3 +4,7 @@ while True:
     try:
         num = float(input("Enter a number: "))
         inputs.append(num)
+
+    except ValueError:
+        print(f"The highest input is {max(inputs)}.")
+        break
